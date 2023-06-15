02961c2c6c93c2b62c465f0e981085412d91a94b
Justiks <vkusniycat@yandex.ru>
2023-06-16 01:21:19 +0400

    new module ConfigValues.py

---
+++ config/ConfigValues.py
@@ -0,0 +1,17 @@
+from configparser import ConfigParser
+
+
+class ConfigValues:
+
+	__config_file = ConfigParser()
+	__config_file.read('config.ini')
+
+	# database section
+	db_name = __config_file.get('database', 'db_name')
+	transactions_to_backup = __config_file.get('database', 'transactions_to_backup')
+
+	# Yadisk section
+	yadisk_jwt = __config_file.get('Yadisk', 'yadisk_jwt')
+
+	# Telegram section
+	telegram_token = __config_file.get('Telegram', 'telegram_token')
