d251db05486a8c5bdb457702497754dec3ff9871
Justiks <vkusniycat@yandex.ru>
2023-06-18 01:19:56 +0400

    Rewrite Timer.py (Optimize)

--- Timer.py
+++ Timer.py
@@ -1,39 +1,38 @@
-from time import sleep
 from threading import Thread
+from time import sleep
 from BaseErrors import ChessError
 from config.ConfigValues import ConfigValues
 
 
 class Timer:
-    def __init__(self, color: bool):
-        self.seconds: int = ConfigValues.game_time
-        self.expires: bool = color
-        self.color = lambda x: "белых" if color else "чёрных"
-        Thread(target=self.start_timer).start()
+	"""class Timer"""
 
-    def start_timer(self):
-        """Запускает цикл событий"""
+	def __init__(self, color: bool):
+		self.color = lambda x: "белых" if color else "чёрных"
+		self.time = ConfigValues.game_time
+		self.ticking = color
+		self.thread = Thread(target=self.timer)
+		self.thread.start()
 
-        self.update_timer()
+	def timer(self):
+		"""Течение таймера"""
 
-    def update_timer(self):
-        """Ведёт время отсчёта"""
+		while self.ticking:
+			sleep(1)
+			self.time = self.time - 1
 
-        while self.seconds:
-            if self.expires:
-                self.seconds -= 1
-                sleep(1)
-        try:
-            assert self.seconds
-        except AssertionError:
-            raise ChessError(ConfigValues.on_end_time.replace('{color}', self.color))
+			try:
+				assert self.time
+			except AssertionError:
+				raise ChessError(ConfigValues.on_end_time.replace('{color}', self.color))
+		return
 
-    def flip_the_timer(self):
-        """Меняет положение таймера (активный/деактивный)"""
+	def flip_the_timer(self):
+		"""Меняет положение таймера (активный/деактивный)"""
 
-        self.expires = not self.expires
+		self.ticking = not self.ticking
 
-    def get_timeset(self) -> str:
-        """:return время в виде таймера на электронных часах"""
-
-        return f"{round(self.seconds / 60)}:{self.seconds % 60}"
+		if self.ticking:
+			Thread(target=self.timer).start()
+			return
+		self.thread.join()
