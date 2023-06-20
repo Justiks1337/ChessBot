90f6603fc0532f797af8f2ad200797db83152420
Justiks <vkusniycat@yandex.ru>
2023-06-20 16:18:14 +0400

    fix log bug

--- telegram/telegram_log/log.py
+++ telegram/telegram_log/log.py
@@ -1,9 +1,10 @@
 from logging import getLogger, FileHandler, Formatter
+from os import path
 
 log = getLogger('telegram_log')
 log.setLevel('DEBUG')
 
-log_handler = FileHandler('main.log', 'a+')
+log_handler = FileHandler(path.join(path.dirname(__file__), 'main.log'), 'a+')
 log_handler.setFormatter(Formatter("%(name)s %(asctime)s %(levelname)s %(message)s"))
 
 
