14626188e5f56c107c74af06d9f7f192210a6d1a
Justiks <vkusniycat@yandex.ru>
2023-07-01 02:10:05 +0400

    bugfix

--- telegram/telegram_core.py
+++ telegram/telegram_core.py
@@ -10,5 +10,7 @@ async def download_user_avatar(bot: Bot, user_id: int):
 
 	user_profile_photo = await bot.get_user_profile_photos(user_id)
 
-	file = await bot.get_file(user_profile_photo.photos[0][0].file_id)
-	await bot.download_file(file.file_path, f'{user_id}.png')
+	if len(user_profile_photo.photos[0]) > 0:
+
+		file = await bot.get_file(user_profile_photo.photos[0][0].file_id)
+		await bot.download_file(file.file_path, f'../avatars/{user_id}.png')
