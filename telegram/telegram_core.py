c15de2cb62ec125421fa083751951a4e0d9d434f
Justiks <vkusniycat@yandex.ru>
2023-07-02 02:15:38 +0400

    ДИЧАЙШИЙ БАГФИКС

--- telegram/telegram_core.py
+++ telegram/telegram_core.py
@@ -20,9 +20,10 @@ async def download_user_avatar(bot: Bot, user_id: int):
 
 def in_blacklist(func):
 	"""Check user in blacklist"""
+
 	async def wrapped(*args, **kwargs):
 		if (await (
-				await connect.request("SELECT user_id FROM blacklist WHERE user_id = ?", (args[1].from_user.username, ))
+				await connect.request("SELECT user_id FROM blacklist WHERE username = ?", (args[1].from_user.username, ))
 		).fetchone()):
 			return await args[0].send_message(args[1].chat.id, ConfigValues.on_blacklsit_message)
 
