5d4b53b2d3a3167f759825c8afe77acf861d9411
--delete <vkusniycat@yandex.ru>
2024-01-14 02:10:55 +0400

    to directories

--- web_django/chessboards/routing.py
+++ web_django/chessboards/routing.py
@@ -1,7 +1,7 @@
 from .consumers import UserWebsocket
-from django.urls import path
+from django.urls import path, re_path, utils
 
 
 websocket_urlpatterns = [
-    path('websocket/games/<str:tag>', UserWebsocket.as_asgi())
+    path('websocket/games/<str:tag>/', UserWebsocket.as_asgi())
 ]
